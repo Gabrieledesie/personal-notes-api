a6d68efe15de38df14fdc5dad931b06beada1a20
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-08-27 13:21:16 +0100

    Updated users/urls.py and users/views.py with register functionality

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
-from rest_framework import generics
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.serializers import ModelSerializer, Serializer, CharField
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
@@ -16,3 +19,30 @@ class RegisterSerializer(ModelSerializer):
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class LoginSerializer(Serializer):
+    username = CharField()
+    password = CharField(write_only=True)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return Response({"message": "Login successful"})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+        })
