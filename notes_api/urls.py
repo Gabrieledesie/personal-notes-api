3ecc098bdada57379a9429151eb42d5a870e0f50
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-08-27 10:56:31 +0100

    Added user registration, login, and JWT token endpoints

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -1,24 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-def home(request):
-    return HttpResponse("Welcome to Personal Notes API")
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Personal Notes API",
-        default_version='v1',
-    ),
-    public=True,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('notes.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
